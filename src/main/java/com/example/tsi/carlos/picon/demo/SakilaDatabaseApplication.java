a7ea42115d99f18a4202ced147b885014f96b087
cpiconsoftwareinstitute <cpicon@softwareinstitute.com>
2022-02-08 15:16:53 +0000

    first commit

---
+++ src/main/java/com/example/tsi/carlos/picon/demo/SakilaDatabaseApplication.java
@@ -0,0 +1,22 @@
+package com.example.tsi.carlos.picon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SakilaDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaDatabaseApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		return String.format("Hello %s!", name);
+
+	}
+}
---
+++ src/test/java/com/example/tsi/carlos/picon/demo/SakilaDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsi.carlos.picon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
