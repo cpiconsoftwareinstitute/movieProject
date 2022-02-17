0c04f1ae0cc3de004d45210a8d5b341c3be95007
cpiconsoftwareinstitute <cpicon@softwareinstitute.com>
2022-02-17 16:15:23 +0000

    add category

---
+++ src/test/java/com/example/tsi/carlos/picon/demoTest/CategoryTest.java
@@ -0,0 +1,18 @@
+package com.example.tsi.carlos.picon.demoTest;
+
+import com.example.tsi.carlos.picon.demo.Category;
+import com.example.tsi.carlos.picon.demo.Language;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CategoryTest {
+
+    @Test
+    void setNameTest() {
+        Category testName = new Category();
+        testName.setName("Drama");
+        assertEquals("Drama", testName.getName(),
+                "The Expected result and the introduced data is not the same");
+    }
+}
